282b6ad937ad7b11fde0fde247156873ec3fcd93
Rido A <98310463+ridoamali@users.noreply.github.com>
2025-04-03 16:39:03 +0700

    perubahan pada frame dan photo

--- README.md
+++ README.md
@@ -7,6 +7,111 @@ Tambahi koneksi ke SPreadsheet
 tambahi konek ke database
 tambahi koneksi ke API
 
+perbaiki /assets/background-homew.jpg menjadi background yang jika baris kodingan ditambah, maka tetap bisa menyesuaikan tingginya
+/assets/pexels-oneprophoto-photo-cinema-16098067-768x960.jpg menjadi seukuran dan pas dengan /assets/couple-frame-mirror.png
+/assets/orn-save-date-1.png berada di sisi kiri dan kanan /assets/couple-frame-mirror.png
+perbaiki layouting agar rapi dan tidak berantakan pada semua jenis ukuran layar baik desktop sampai mobile devices
+perbaiki semua class "translate-" yang menjadikan tampilan tidak rapi pada ukuran layar mobile device tertentu
+
+
+
+backup
+{/* Section image with frame and mirror */}
+            <div className='relative h-screen'>
+              {/* Background Layer */}
+              <div className='absolute inset-0 bg-[url(/assets/background-homew.jpg)] bg-cover bg-center bg-no-repeat h-screen'></div>
+              
+              {/* Photo Layer Women - Positioned to fit inside frame */}
+              <div className='absolute inset-0 flex items-center justify-center'>
+                <div className='w-[50%] h-[35%] bg-[url(/assets/pexels-oneprophoto-photo-cinema-16098067-768x960.jpg)] bg-cover bg-center bg-no-repeat'></div>
+              </div>
+              
+              {/* Frame Layer */}
+              <div className='absolute inset-0 bg-[url(/assets/couple-frame-mirror.png)] w-[60%] bg-contain bg-center bg-no-repeat mx-auto'></div>
+              
+              {/* Ornament Layer */}
+              <div className='absolute inset-0 flex items-center justify-center'>
+                <div className='absolute inset-0 bg-[url(/assets/orn-save-date-1.png)] bg-contain bg-no-repeat translate-x-10 translate-y-50 size-[40%]'></div>
+                <div className='absolute inset-0 bg-[url(/assets/orn-save-date-1.png)] scale-x-[-1] bg-contain bg-no-repeat translate-x-48 translate-y-50 size-[40%]'></div>
+              </div>
+              
+              {/* Text Overlay - Women */}
+              <div className='relative z-10 flex flex-col gap-2 text-center translate-y-140'>
+                <span className='text-xl text-black font-thin'>PUTRI</span>
+                <span className='text-xl text-black font-thin'>FULANAH</span>
+                <span className='text-xl text-black font-thin'>The Daughter of</span>
+                <span className='text-xl text-black font-thin'>Mr. Reynald Robert</span>
+                <span className='text-xl text-black font-thin'>& Mrs. Felia Lenita</span>
+              </div>
+
+              {/* AND Text */}
+              <div className='relative z-10 flex flex-col gap-2 text-center translate-y-160'>
+                <span className='text-xl text-black font-thin'>AND</span>
+              </div>
+
+              {/* Photo Layer Men - Positioned to fit inside frame */}
+              <div className='absolute inset-0 flex items-center justify-center translate-y-180'>
+                <div className='w-[50%] h-[35%] bg-[url(/assets/pexels-oneprophoto-photo-cinema-16098066-Copy-768x1126.jpg)] bg-cover bg-center bg-no-repeat'></div>
+              </div>
+              
+              {/* Frame Layer - Men */}
+              <div className='absolute inset-0 bg-[url(/assets/couple-frame-mirror.png)] w-[60%] bg-contain bg-center bg-no-repeat mx-auto translate-y-180'></div>
+              
+              {/* Ornament Layer - Men */}
+              <div className='absolute inset-0 flex items-center justify-center translate-y-180'>
+                <div className='absolute inset-0 bg-[url(/assets/orn-save-date-1.png)] bg-contain bg-no-repeat translate-x-10 translate-y-50 size-[40%]'></div>
+                <div className='absolute inset-0 bg-[url(/assets/orn-save-date-1.png)] scale-x-[-1] bg-contain bg-no-repeat translate-x-48 translate-y-50 size-[40%]'></div>
+              </div>
+
+              {/* Text Overlay - Men */}
+              <div className='relative z-10 flex flex-col gap-2 text-center translate-y-280'>
+                <span className='text-xl text-black font-thin'>PUTRA</span>
+                <span className='text-xl text-black font-thin'>FULAN</span>
+                <span className='text-xl text-black font-thin'>The Son of</span>
+                <span className='text-xl text-black font-thin'>Mr. Reynald Robert</span>
+                <span className='text-xl text-black font-thin'>& Mrs. Felia Lenita</span>
+              </div>
+            </div>
+
+                        {/* Section Our Journey */}
+            <div className='bg-stone-400/50 h-fit flex flex-col items-center justify-center mt-[245%] p-4 pb-10'>
+              <h1 className='text-3xl font-thin mb-8'>Our Journey</h1>
+              <div className='w-full max-w-2xl mb-8'>
+                <div className='w-full h-[220px] bg-[url(/assets/pexels-oneprophoto-photo-cinema-16098072-768x442.jpg)] bg-cover bg-center bg-no-repeat rounded-lg'></div>
+              </div>
+              <div className='max-w-2xl text-justify'>
+                <span className='text-sm leading-relaxed'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
+                </span>
+                <span className='text-sm leading-relaxed'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
+                </span>
+                <span className='text-sm leading-relaxed'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
+                </span>
+              </div>
+            </div>
+
+            
+            {/* Section Wedding Event */}
+            <div className='bg-stone-400/50 h-fit flex flex-col items-center justify-center -mt-4 p-4 pb-10'>
+              <h1 className='text-3xl font-thin mb-8'>Our Journey</h1>
+              <div className='w-full max-w-2xl mb-8'>
+                <div className='w-full h-[220px] bg-[url(/assets/pexels-oneprophoto-photo-cinema-16098072-768x442.jpg)] bg-cover bg-center bg-no-repeat rounded-lg'></div>
+              </div>
+              <div className='max-w-2xl text-justify'>
+                <span className='text-sm leading-relaxed'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
+                </span>
+                <span className='text-sm leading-relaxed'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
+                </span>
+                <span className='text-sm leading-relaxed'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
+                </span>
+              </div>
+            </div>
+
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.tsx
+++ src/App.tsx
@@ -28,101 +28,100 @@ function App() {
           
           {/* Scrollable content */}
           <div className='flex flex-col gap-4'>
+
             {/* Section image with frame and mirror */}
-            <div className='relative h-screen'>
+            <div className='relative min-h-screen w-full flex flex-col items-center justify-center py-20'>
               {/* Background Layer */}
-              <div className='absolute inset-0 bg-[url(/assets/background-homew.jpg)] bg-cover bg-center bg-no-repeat h-[250%]'></div>
+              <div className='absolute inset-0 bg-[url(/assets/background-homew.jpg)] bg-cover bg-center bg-no-repeat w-full h-full'></div>
               
-              {/* Photo Layer Women - Positioned to fit inside frame */}
-              <div className='absolute inset-0 flex items-center justify-center'>
-                <div className='w-[50%] h-[35%] bg-[url(/assets/pexels-oneprophoto-photo-cinema-16098067-768x960.jpg)] bg-cover bg-center bg-no-repeat'></div>
-              </div>
-              
-              {/* Frame Layer */}
-              <div className='absolute inset-0 bg-[url(/assets/couple-frame-mirror.png)] w-[60%] bg-contain bg-center bg-no-repeat mx-auto'></div>
-              
-              {/* Ornament Layer */}
-              <div className='absolute inset-0 flex items-center justify-center'>
-                <div className='absolute inset-0 bg-[url(/assets/orn-save-date-1.png)] bg-contain bg-no-repeat translate-x-10 translate-y-50 size-[40%]'></div>
-                <div className='absolute inset-0 bg-[url(/assets/orn-save-date-1.png)] scale-x-[-1] bg-contain bg-no-repeat translate-x-48 translate-y-50 size-[40%]'></div>
-              </div>
-              
-              {/* Text Overlay - Women */}
-              <div className='relative z-10 flex flex-col gap-2 text-center translate-y-140'>
-                <span className='text-xl text-black font-thin'>PUTRI</span>
-                <span className='text-xl text-black font-thin'>FULANAH</span>
-                <span className='text-xl text-black font-thin'>The Daughter of</span>
-                <span className='text-xl text-black font-thin'>Mr. Reynald Robert</span>
-                <span className='text-xl text-black font-thin'>& Mrs. Felia Lenita</span>
-              </div>
+              {/* Main Content Container */}
+              <div className='relative w-full max-w-4xl mx-auto px-4 flex flex-col items-center'>
+                {/* Frame and Photo Container */}
+                <div className='relative w-full aspect-[3/4] max-w-lg'>
+                  {/* Photo Layer - Positioned to fit inside frame */}
+                  <div className='absolute inset-0 flex items-center justify-center'>
+                    <div className='w-[65%] h-[58%] bg-[url(/assets/pexels-oneprophoto-photo-cinema-16098067-768x960.jpg)] bg-cover bg-center bg-no-repeat'></div>
+                  </div>
+                  
+                  {/* Frame Layer */}
+                  <div className='absolute inset-0 bg-[url(/assets/couple-frame-mirror.png)] bg-contain bg-center bg-no-repeat w-full h-[80%] mt-12'></div>
+                  
+                  {/* Ornament Layer */}
+                  <div className='absolute inset-0 w-full'>
+                    {/* Left Ornament */}
+                    <div className='absolute left-0 top-1/2 -translate-x-36 md:-translate-x-1/2 -translate-y-1/2'>
+                      <div className='w-96 h-80 md:w-40 md:h-40 bg-[url(/assets/orn-save-date-1.png)] bg-contain bg-center bg-no-repeat'></div>
+                    </div>
+                    {/* Right Ornament */}
+                    <div className='absolute right-0 top-1/2 translate-x-36 md:translate-x-1/5 -translate-y-1/2'>
+                      <div className='w-96 h-80 md:w-20 md:h-40 bg-[url(/assets/orn-save-date-1.png)] scale-x-[-1] bg-contain bg-center bg-no-repeat'></div>
+                    </div>
+                  </div>
+                </div>
 
-              {/* AND Text */}
-              <div className='relative z-10 flex flex-col gap-2 text-center translate-y-160'>
-                <span className='text-xl text-black font-thin'>AND</span>
-              </div>
+                {/* Text Content */}
+                <div className='mt-8 text-center'>
+                  {/* Text Overlay - Women */}
+                  <div className='mb-6'>
+                    <p className='text-xl md:text-2xl text-black font-thin'>PUTRI</p>
+                    <p className='text-xl md:text-2xl text-black font-thin'>FULANAH</p>
+                    <p className='text-xl md:text-2xl text-black font-thin mt-2'>The Daughter of</p>
+                    <p className='text-xl md:text-2xl text-black font-thin'>Mr. Reynald Robert</p>
+                    <p className='text-xl md:text-2xl text-black font-thin'>& Mrs. Felia Lenita</p>
+                  </div>
 
-              {/* Photo Layer Men - Positioned to fit inside frame */}
-              <div className='absolute inset-0 flex items-center justify-center translate-y-180'>
-                <div className='w-[50%] h-[35%] bg-[url(/assets/pexels-oneprophoto-photo-cinema-16098066-Copy-768x1126.jpg)] bg-cover bg-center bg-no-repeat'></div>
-              </div>
-              
-              {/* Frame Layer - Men */}
-              <div className='absolute inset-0 bg-[url(/assets/couple-frame-mirror.png)] w-[60%] bg-contain bg-center bg-no-repeat mx-auto translate-y-180'></div>
-              
-              {/* Ornament Layer - Men */}
-              <div className='absolute inset-0 flex items-center justify-center translate-y-180'>
-                <div className='absolute inset-0 bg-[url(/assets/orn-save-date-1.png)] bg-contain bg-no-repeat translate-x-10 translate-y-50 size-[40%]'></div>
-                <div className='absolute inset-0 bg-[url(/assets/orn-save-date-1.png)] scale-x-[-1] bg-contain bg-no-repeat translate-x-48 translate-y-50 size-[40%]'></div>
-              </div>
+                  {/* AND Text */}
+                  <div className='mb-6'>
+                    <p className='text-xl md:text-2xl text-black font-thin'>AND</p>
+                  </div>
+                </div>
 
-              {/* Text Overlay - Men */}
-              <div className='relative z-10 flex flex-col gap-2 text-center translate-y-280'>
-                <span className='text-xl text-black font-thin'>PUTRA</span>
-                <span className='text-xl text-black font-thin'>FULAN</span>
-                <span className='text-xl text-black font-thin'>The Son of</span>
-                <span className='text-xl text-black font-thin'>Mr. Reynald Robert</span>
-                <span className='text-xl text-black font-thin'>& Mrs. Felia Lenita</span>
+
+                {/* Frame and Photo Container COWOK */}
+                <div className='relative w-full aspect-[3/4] max-w-lg'>
+                  {/* Photo Layer - Positioned to fit inside frame */}
+                  <div className='absolute inset-0 flex items-center justify-center'>
+                    <div className='w-[65%] h-[58%] bg-[url(/assets/pexels-oneprophoto-photo-cinema-16098066-Copy-699x1024.jpg)] bg-cover bg-center bg-no-repeat'></div>
+                  </div>
+                  
+                  {/* Frame Layer */}
+                  <div className='absolute inset-0 bg-[url(/assets/couple-frame-mirror.png)] bg-contain bg-center bg-no-repeat w-full h-[80%] mt-12'></div>
+                  
+                  {/* Ornament Layer */}
+                  <div className='absolute inset-0 w-full'>
+                    {/* Left Ornament */}
+                    <div className='absolute left-0 top-1/2 -translate-x-36 md:-translate-x-1/2 -translate-y-1/2'>
+                      <div className='w-96 h-80 md:w-40 md:h-40 bg-[url(/assets/orn-save-date-1.png)] bg-contain bg-center bg-no-repeat'></div>
+                    </div>
+                    {/* Right Ornament */}
+                    <div className='absolute right-0 top-1/2 translate-x-36 md:translate-x-1/5 -translate-y-1/2'>
+                      <div className='w-96 h-80 md:w-20 md:h-40 bg-[url(/assets/orn-save-date-1.png)] scale-x-[-1] bg-contain bg-center bg-no-repeat'></div>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Text Content */}
+                <div className='mt-8 text-center'>
+                  {/* Text Overlay - Women */}
+                  <div className='mb-6'>
+                    <p className='text-xl md:text-2xl text-black font-thin'>PUTRI</p>
+                    <p className='text-xl md:text-2xl text-black font-thin'>FULANAH</p>
+                    <p className='text-xl md:text-2xl text-black font-thin mt-2'>The Daughter of</p>
+                    <p className='text-xl md:text-2xl text-black font-thin'>Mr. Reynald Robert</p>
+                    <p className='text-xl md:text-2xl text-black font-thin'>& Mrs. Felia Lenita</p>
+                  </div>
+
+                  {/* AND Text */}
+                  <div className='mb-6'>
+                    <p className='text-xl md:text-2xl text-black font-thin'>AND</p>
+                  </div>
+                </div>
+
+
               </div>
             </div>
 
-            {/* Section Our Journey */}
-            <div className='bg-stone-400/50 h-fit flex flex-col items-center justify-center mt-[245%] p-4 pb-10'>
-              <h1 className='text-3xl font-thin mb-8'>Our Journey</h1>
-              <div className='w-full max-w-2xl mb-8'>
-                <div className='w-full h-[220px] bg-[url(/assets/pexels-oneprophoto-photo-cinema-16098072-768x442.jpg)] bg-cover bg-center bg-no-repeat rounded-lg'></div>
-              </div>
-              <div className='max-w-2xl text-justify'>
-                <span className='text-sm leading-relaxed'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
-                </span>
-                <span className='text-sm leading-relaxed'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
-                </span>
-                <span className='text-sm leading-relaxed'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
-                </span>
-              </div>
-            </div>
 
-            
-            {/* Section Wedding Event */}
-            <div className='bg-stone-400/50 h-fit flex flex-col items-center justify-center -mt-4 p-4 pb-10'>
-              <h1 className='text-3xl font-thin mb-8'>Our Journey</h1>
-              <div className='w-full max-w-2xl mb-8'>
-                <div className='w-full h-[220px] bg-[url(/assets/pexels-oneprophoto-photo-cinema-16098072-768x442.jpg)] bg-cover bg-center bg-no-repeat rounded-lg'></div>
-              </div>
-              <div className='max-w-2xl text-justify'>
-                <span className='text-sm leading-relaxed'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
-                </span>
-                <span className='text-sm leading-relaxed'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
-                </span>
-                <span className='text-sm leading-relaxed'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, beatae inventore atque rem ipsam amet doloremque illo. Quae vitae porro tempore cum laudantium iste vero voluptatum quibusdam officia dolor nemo totam debitis sequi,
-                </span>
-              </div>
-            </div>
           </div>
         </div>
       </div>
